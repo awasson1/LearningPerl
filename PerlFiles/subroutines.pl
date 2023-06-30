6ae9c95b40cf5aa5b8ffa8e90a07baeca267b253
awasson1 <Anthonywasson1@gmail.com>
2023-06-29 22:31:39 -0700

    Update subroutines.pl
    added different argument types. Tested return keyword.

--- PerlFiles/subroutines.pl
+++ PerlFiles/subroutines.pl
@@ -7,6 +7,7 @@ sub printHello
 {
     print "Hello!\n";
 }
+printHello();
 
 sub makeUpperCase
 {
@@ -14,5 +15,39 @@ sub makeUpperCase
     print uc "$string \n";
 }
 
-printHello();
-makeUpperCase("case");
+
+sub printHash
+{
+    my (%hash) = @_;
+
+    for my $key (keys %hash)
+    {
+        print $hash {$key}, "\n";
+    }
+}
+
+my %hashOfNames = qw(
+    zero john
+    one doe
+    two jim
+    three bo
+);
+printHash(%hashOfNames);
+
+sub sumsArrayElements
+{
+    my (@array) = @_;
+
+    my $total = 0;
+
+    foreach (@array)
+    {
+        $total += $_;
+    }
+
+    return $total;
+}
+
+my @arrOfNums = (1, 2, 3, 4, 5);
+
+print sumsArrayElements(@arrOfNums), "\n";
