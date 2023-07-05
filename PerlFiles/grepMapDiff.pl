9c298ab3bd7b08d9546c8ac43e173322de9151c3
awasson1 <Anthonywasson1@gmail.com>
2023-07-04 23:22:00 -0700

    Update grepMapDiff.pl
    testing more differences between grep and map

--- PerlFiles/grepMapDiff.pl
+++ PerlFiles/grepMapDiff.pl
@@ -5,8 +5,20 @@ use Data::Dumper;
 # exploring differences between map and grep in perl
 
 my @array = (-2, -1, 0, 1, 2, 3, 4, 5);
-my @myMap = map { $_ + 1 } @array;
-my @myGrep = grep { $_ + 1 } @array;
 
-print Dumper(\@myMap);
-print Dumper(\@myGrep);
+# add one
+my @myMapAdd = map { $_ + 1 } @array;
+my @myGrepAdd = grep { $_ + 1 } @array;
+
+print "+ 1: \n";
+print Dumper(\@myMapAdd);
+print Dumper(\@myGrepAdd);
+
+# is even?
+
+my @myMapModulo = map { $_ % 2 == 0 } @array;
+my @myGrepModulo = grep { $_ % 2 == 0 } @array;
+
+print "% 2 == 0: \n";
+print Dumper(\@myMapModulo);
+print Dumper(\@myGrepModulo);
