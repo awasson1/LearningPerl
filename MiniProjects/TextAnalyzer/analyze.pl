e0ad9e34b86d59b2343f92c48ddcdf86f7ad79eb
awasson1 <Anthonywasson1@gmail.com>
2023-07-07 23:00:13 -0700

    Implement user input and file not found error handling

--- MiniProjects/TextAnalyzer/analyze.pl
+++ MiniProjects/TextAnalyzer/analyze.pl
@@ -3,3 +3,19 @@ use warnings;
 
 # This program takes a text file as input and displays statistics about the contents of the file.
 
+print "Enter file name (e.g. Hello.txt):\n";
+
+chomp(my $fileName = <STDIN>);
+my $path = "./$fileName";
+
+if (-f $path)
+{
+    open(my $fileHandle, "<", $path);
+
+    # TODO: Implement analysis
+    print <$fileHandle>, "\n";
+}
+else
+{
+    print STDERR "Please enter a valid file name"
+}
