03c064b1f87cb908cd65b87f81911debd7780af0
awasson1 <Anthonywasson1@gmail.com>
2023-07-09 22:33:42 -0700

    Read data from file into array, and calculate character count in text analyzer

--- MiniProjects/TextAnalyzer/analyze.pl
+++ MiniProjects/TextAnalyzer/analyze.pl
@@ -8,12 +8,22 @@ print "Enter file name (e.g. Hello.txt):\n";
 chomp(my $fileName = <STDIN>);
 my $path = "./$fileName";
 
+# -f checks if file exists, -e checks if the directory exists
 if (-f $path)
 {
     open(my $fileHandle, "<", $path);
+    chomp(my @lines = <$fileHandle>);
+    close($fileHandle);
 
     # TODO: Implement analysis
-    print <$fileHandle>, "\n";
+    my $chars = 0;
+    foreach(@lines)
+    {
+        $chars += length($_);
+    }
+
+    print "File statistics:\n";
+    print "Characters: $chars\n";
 }
 else
 {
