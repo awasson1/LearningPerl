f1698b2334852b2676f3d20f380f11e78048cd84
awasson1 <Anthonywasson1@gmail.com>
2023-07-13 22:43:08 -0700

    Add average word length calculation and print formatting

--- MiniProjects/TextAnalyzer/analyze.pl
+++ MiniProjects/TextAnalyzer/analyze.pl
@@ -23,14 +23,18 @@ if (-f $path)
     foreach(@lines)
     {
         $chars += length($_);
-        $words += scalar(split(/\s+/, $_)); #words are assumed to be split by one or more spaces
+        $words += scalar(split(/\s+/, $_)); # words are assumed to be split by one or more spaces
         $lines++;
     }
+
+    my $avgWordLength = $chars / $words;
+    $avgWordLength = int($avgWordLength * 100) / 100; # truncate to two decimal places to make it readable
 
     print "File statistics:\n";
     print "Characters: $chars\n";
     print "Words: $words\n";
     print "Lines: $lines\n";
+    print "Average Word Length: $avgWordLength\n";
 }
 else
 {
